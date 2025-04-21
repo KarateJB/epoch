bbe2b2a6f6e8148246ce1ee1730b8a562321e402
karatejb <ppippenkimo@gmail.com>
2025-04-21 13:26:59 +0800

    Convert UTC datetime to unix timestamp

--- timestamp_converter.go
+++ timestamp_converter.go
@@ -17,3 +17,21 @@ func ConvertMicrosecondsToUTC(microseconds int64) string {
 	t := time.Unix(seconds, nanoseconds).UTC()
 	return t.Format("2006-01-02T15:04:05.000000Z")
 }
+
+// ConvertUTCToMilliseconds converts a UTC datetime string to epoch time in milliseconds.
+func ConvertUTCToMilliseconds(utc string) (int64, error) {
+	t, err := time.Parse(time.RFC3339, utc)
+	if err != nil {
+		return 0, err
+	}
+	return t.UnixNano() / 1e6, nil
+}
+
+// ConvertUTCToSeconds converts a UTC datetime string to epoch time in seconds.
+func ConvertUTCToSeconds(utc string) (int64, error) {
+	t, err := time.Parse(time.RFC3339, utc)
+	if err != nil {
+		return 0, err
+	}
+	return t.Unix(), nil
+}
--- timestamp_converter_test.go
+++ timestamp_converter_test.go
@@ -21,3 +21,27 @@ func TestConvertMicrosecondsToUTC(t *testing.T) {
 		t.Errorf("ConvertMicrosecondsToUTC(%d) = %s; want %s", microseconds, result, expected)
 	}
 }
+
+func TestConvertUTCToMilliseconds(t *testing.T) {
+	utc := "2021-10-01T07:20:00.123Z" // Example UTC datetime string
+	expected := int64(1633072800123) // Expected epoch time in milliseconds
+	result, err := ConvertUTCToMilliseconds(utc)
+	if err != nil {
+		t.Errorf("ConvertUTCToMilliseconds(%s) returned error: %v", utc, err)
+	}
+	if result != expected {
+		t.Errorf("ConvertUTCToMilliseconds(%s) = %d; want %d", utc, result, expected)
+	}
+}
+
+func TestConvertUTCToSeconds(t *testing.T) {
+	utc := "2021-10-01T07:20:03Z" // Example UTC datetime string
+	expected := int64(1633072803) // Expected epoch time in seconds
+	result, err := ConvertUTCToSeconds(utc)
+	if err != nil {
+		t.Errorf("ConvertUTCToSeconds(%s) returned error: %v", utc, err)
+	}
+	if result != expected {
+		t.Errorf("ConvertUTCToSeconds(%s) = %d; want %d", utc, result, expected)
+	}
+}
